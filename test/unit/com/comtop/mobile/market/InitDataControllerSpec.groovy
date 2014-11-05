f23e4548f020ab0e7a98ea85512ac34df4ee66ad
shake'shome <shake822@qq.com>
2014-11-05 23:31:13 +0800

    test

---
+++ test/unit/com/comtop/mobile/market/InitDataControllerSpec.groovy
@@ -0,0 +1,20 @@
+package com.comtop.mobile.market
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(InitDataController)
+class InitDataControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
